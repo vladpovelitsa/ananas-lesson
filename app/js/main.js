12aefb88318389334e31e03b0e4dcedc23832235
bmatveyka <bmatveyka@gmail.com>
2021-12-13 23:07:47 +0200

    work in progress

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+$(".about_desc").slick({
+	arrows: false,
+	asNavFor: ".about_slider"
+})
+$(".about_slider").slick({
+	asNavFor: ".about_desc"
+})
+$(".location_desc").slick({
+	arrows: false,
+	asNavFor: ".location_slider"
+})
+$(".location_slider").slick({
+	asNavFor: ".location_desc"
+})
+
+$(".lobby_slider").slick({
+})
+$(".gallery_slider").slick({
+})
